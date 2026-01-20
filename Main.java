05a85808401d0d1072d4bf4c131ba431011232c4
Endika <98endika@gmail.com>
2026-01-20 11:55:26 +0100

    cambio de saludo main

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo! soy endi que tengas un buen dia!");
+        System.out.println("Hola mundo! soy endi desde la version principal");
     }
 }
