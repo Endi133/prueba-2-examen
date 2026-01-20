bbf87451c7abaece593034d905b93007d8a46981
Endika <98endika@gmail.com>
2026-01-20 11:52:11 +0100

    mensaje de despedida añadido

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo! soy endi");
+        System.out.println("Hola mundo! soy endi que tengas un buen dia!");
     }
 }
